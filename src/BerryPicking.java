1120073b958b7a632aaeb56d06624e1e4a594f4a
Sauman Das <saumand9424@gmail.com>
2020-12-07 10:01:18 -0500

    added Berry

--- src/BerryPicking.java
+++ src/BerryPicking.java
@@ -1,8 +1,16 @@
+/******************************
+ * Sauman Das
+ * December 7, 2020
+ * USACO 2020 January Silver
+ * Problem 1. Berry Picking
+ * All Correct! :)
+ ******************************/
+
 import java.util.*; import java.io.*;
 public class BerryPicking {
-    static final String file = "io//berries";
+    static final String file = "berries";
     static List<Integer> basks = new ArrayList<>();
-    static PriorityQueue<Integer> toChange = new PriorityQueue<>();
+    static PriorityQueue<Integer> toChange;
     static int n, k;
     public static void main(String[] args) throws IOException{
         BufferedReader f = new BufferedReader(new FileReader(file + ".in"));
@@ -16,34 +24,67 @@ public class BerryPicking {
         }
         Collections.sort(basks); Collections.reverse(basks);
 //        System.out.println(basks);
-        for (int i = 0; i < k; i++){
-            toChange.add(-1*basks.get(i));
-            least = -1*basks.get(i);
+        toChange = new PriorityQueue<>(n, new NegSort());
+        for (int i = 0; i < n; i++){
+            toChange.add(basks.get(i));
+//            least = basks.get(i);
         }
-//        System.out.println(toChange);
-        out.println(answer());
+        int maxB = toChange.peek();
+        int maxleast = -1;
+        for (int i = maxB; i >= 1; i--){
+            int least = getLeast(i, new PriorityQueue<>(toChange));
+            maxleast = Math.max(maxleast, least);
+        }
+        out.println(maxleast);
+//        System.out.println(answer());
         f.close();out.close();
     }
-    static int least;
-    static int answer(){
-        if (toChange.peek()/2 >= least){
-            return kby2();
+    static int getLeast(int b, PriorityQueue<Integer> pq){
+        boolean elsie = true;
+        ArrayList<Integer> buckets = new ArrayList<>();
+        for (int i = 0; i < k; i++){
+            if (i < k/2){
+                if (pq.peek() < b) return -1;
+                int next = pq.remove();
+                buckets.add(b);
+                pq.add(next-b);
+            }
+            else{
+                int next = pq.remove();
+                if (next >= b){
+                    buckets.add(b);
+                    pq.add(next-b);
+                }
+                else{
+                    buckets.add(next);
+                    pq.add(0);
+                }
+            }
         }
-        int next = toChange.remove();
-        int n1 = next/2;
-        int n2 = next-n1;
-        toChange.add(n1); toChange.add(n2);
-        least = n1;
-        return -1*answer();
+        return kby2(buckets);
     }
-    static int kby2(){
+//    static int answer(){
+//        if (toChange.peek()/2 <= least){
+//            return kby2();
+//        }
+//        int next = toChange.remove();
+//        int n1 = next/2;
+//        int n2 = next-n1;
+//        toChange.add(n1); toChange.add(n2);
+//        least = n1;
+//        return answer();
+//    }
+    static int kby2(ArrayList<Integer> arr){
         int sum = 0;
         for (int i = 0; i < k; i++){
-            int next = toChange.remove();
+
             if (i >= (k/2)){
-                sum += next;
+                sum += arr.get(i);
             }
         }
         return sum;
+    }
+    static class NegSort implements Comparator<Integer>{
+        public int compare(Integer a, Integer b){return Integer.compare(b, a);}
     }
 }
